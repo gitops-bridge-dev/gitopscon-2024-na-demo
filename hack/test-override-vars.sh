e5e37f171a2fc997086c1b32a9bf9d2ddacb2f74
Carlos Santana <csantana23@gmail.com>
2024-04-03 21:31:14 -0400

    use proper project name
    Signed-off-by: Carlos Santana <csantana23@gmail.com>

--- hack/test-override-vars.sh
+++ hack/test-override-vars.sh
@@ -4,6 +4,6 @@ export DEBUG=true
 export GITHUB_REPOSITORY_OWNER=csantanapr
 export GITHUB_REPOSITORY=csantanapr/gitopscon-2024-na-demo
 export PULUMI_ACCOUNT=csantanapr
-export PULUMI_PROJECT=gitopscon-2024-na-demo-carlos
+export PULUMI_PROJECT=gitopscon-2024-na-demo
 export PULUMI_STACK_NAME=hub
 export VELERO_BACKUP_BUCKET=gitopscon-na-24-velero-backups-carlos
